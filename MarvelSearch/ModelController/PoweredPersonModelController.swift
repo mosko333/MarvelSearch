5d8e0fa37033e10306d0bdfe65e710798197a608
AdamAM <adamam@gmail.com>
2018-05-30 15:34:06 -0600

    Finnished model

---
+++ MarvelSearch/ModelController/PoweredPersonModelController.swift
@@ -0,0 +1,9 @@
+//
+//  PoweredPersonModelController.swift
+//  MarvelSearch
+//
+//  Created by Adam on 30/05/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+import Foundation
