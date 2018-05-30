bc2b7ea4807a9ac964a2c6e02fd6ecaebb9fedb8
AdamAM <adamam@gmail.com>
2018-05-30 16:00:10 -0600

    Added constants and hash key in PoweredPersonModelController

--- MarvelSearch/ModelController/PoweredPersonModelController.swift
+++ MarvelSearch/ModelController/PoweredPersonModelController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 Adam Moskovich. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class PoweredPersonModelController {
+    struct Constants {
+        static let baseURL = URL(string: "https://gateway.marvel.com/")
+        static let publicKey = "86c7f8a803e9667decddc37f510c853a"
+        static let privateKey = "8f3ba9c012c24d233c21939f2886f9c15fd43ff3"
+        static let setTimestamp = "1527711881"
+        static let setHashKey = "6b51321baae6388e7ba9c0e76d4249e8"
+        static let dynamicHashKeyGenerated = "\(Constants.setTimestamp)\(Constants.privateKey)\(Constants.publicKey)".utf8.md5.description
+    }
+    // https://gateway.marvel.com/v1/public/characters?name=beast&ts=1527711881&apikey=86c7f8a803e9667decddc37f510c853a&hash=6b51321baae6388e7ba9c0e76d4249e8
+    
+    static func fetchPoweredPersonWith(name: String, completion: @escaping ((PoweredPerson?) -> Void)) {
+        // URL
+        
+        // Request
+        
+        // URLSessionDataTask + Resume + Decode
+    }
+}
